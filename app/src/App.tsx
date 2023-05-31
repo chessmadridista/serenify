7a079e4b52ec66b967525e93c95b00638a508e49
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-05-31 13:01:32 +0530

    Remove unnecessary elements.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 import TrashRoom from './components/trash room/TrashRoom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
