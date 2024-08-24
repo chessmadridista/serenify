c6beca1f0994d964c7c758d5d87471155eb673fb
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-08-24 20:15:59 +0530

    feat: add logo to the bottom right side of the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css'
 import TrashRoom from './components/trash room/TrashRoom';
+import logo from './assets/logo.png'
+import { Box } from '@mui/material';
 
 function App() {
   return (
     <>
       <TrashRoom />
+      <Box
+        component="img"
+        sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+          position: 'fixed',
+          bottom: 0,
+          right: 0,
+        }}
+        alt="Serenify logo."
+        src={logo}
+      />
     </>
   )
 }
